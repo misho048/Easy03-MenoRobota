38e3db4cf325203afb4185d46fa1df06e79a4ad1
Michal Matis <miso.matis@gmail.com>
2019-04-05 14:31:21 +0200

    done, i guess

---
+++ RobotName/RobotName/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Robot r1 = new Robot();
+            r1.Describe();
+            r1.FactoryReset();
+            r1.Describe();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RobotName/RobotName/Robot.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotName
+{
+    class Robot
+    {
+        private string _name;
+        Random random = new Random();
+
+        public Robot()
+        {
+            NameGenerator();
+        }
+
+        private void NameGenerator()
+        {
+            
+            string something = "QWERTYUIOPASDFGHJKLZXCVBNM";
+
+            _name = $"{something[random.Next(0, something.Length)].ToString()}{something[random.Next(0, something.Length)].ToString()}" +
+                $"{random.Next(0,9).ToString()}{random.Next(0, 9).ToString()}{random.Next(0, 9).ToString()}";
+
+        }
+
+        public void FactoryReset()
+        {
+            NameGenerator();
+        }
+
+
+        public void Describe()
+        {
+            Console.WriteLine($"Hi my name is {_name}");
+        }
+    }
+}
